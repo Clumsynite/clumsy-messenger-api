e7aaa346d5a0fdd2de7d55570607136a66c986db
Rishabh Pathak <clusmsyknight@gmail.com>
2021-03-29 23:19:11 +0530

    removed comments

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -5,27 +5,6 @@ const User = require("../models/User.js");
 
 const SECRET_KEY = process.env.SECRET;
 
-// exports.encode = async (req, res, next) => {
-//   try {
-//     const { username, password } = req.body;
-//     const user = await User.findOne({ username, password });
-
-//     console.log("User ", user)
-//     const payload = {
-//       userId: user._id,
-//       fullname: `${user.firstname} ${user.lastname}`,
-//       ...user,
-//       password: "*****",
-//     };
-//     const authToken = jwt.sign(payload, SECRET_KEY);
-//     req.authToken = authToken;
-//     console.log("Auth token: ", authToken)
-//     next();
-//   } catch (error) {
-//     return res.status(400).json({ success: false, error });
-//   }
-// };
-
 exports.decode = async (req, res, next) => {
   const bearerHeader = req.headers["authorization"];
   const token = req.cookies.auth;
